febdb174b8283b1f1135c1dcea0a559ba85c6ecd
rades12340 <rades12340@gmail.com>
2018-12-24 01:17:38 +0100

    after css modules

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import styles from "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.App}>
+        <header className={styles.Header}>
+          <img src={logo} className={styles.Logo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className={styles.Link}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
